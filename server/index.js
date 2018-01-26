e3e5342e93b1e796cf7ae29ce1acaf6421f40fbd
Kekoa Wilson <kekoawilson20@gmail.com>
2018-01-26 12:52:58 -0700

    added auth0

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const app = require('express')()
         ,  bodyParser = require('body-parser')
         ,  cors = require('cors')
         ,  port = 3001 || process.env.SERVER_PORT
-        ,  massive = require('massive');
+        ,  massive = require('massive')
+        , passport = require('passport')
+        , Auth0Strategy = require('passport-auth0');
         let  sampleData = [
             { that: 'yo', theOther: 'yay' }, { that: 'hi', theOther: 'hey' }, { that: 'no', theOther: 'nay' }, { that: 'bye', theOther: 'bae' }, { that: 'so', theOther: 'stay' },
         ]
@@ -11,6 +13,43 @@ const app = require('express')()
 // 76F
 app.use(bodyParser.json())
 app.use(cors())
+
+// 78B
+passport.use( new Auth0Strategy( {
+    domain: process.env.AUTH_DOMAIN,
+    clientID: process.env.AUTH_CLIENT_ID,
+    clientSecret: process.env.AUTH_CLIENT_SECRET,
+    callbackURL: process.env.AUTH_CALLBACK_URL,
+}, function( acessToken, refreshToken, extraParams, profile, done ) {
+    const db = app.get( 'db' )
+    let userData = profile._json, // coming from google. Find out what it is for FB and other ones you want to use for login.
+    auth_id = userData.user_id.split('|')[1]
+    db.find_user( [auth_id] ).then( user => {
+        if ( user[0] ) {
+            return done( null, user[0].id )
+        } else {
+            db.create_user( [ userData.family_name, userData.given_name, userData.email, auth_id, userData.picture ] )
+            .then( user => done( null, user[0].id ) )
+        }
+    } )
+
+} ) )
+
+app.get( '/auth', passport.authenticate( 'auth0' ) )
+app.get( '/auth/callback', passport.authenticate( 'auth0', {
+    successRedirect: 'http://localhost:3001',
+    failureRedirect: '/auth'
+} ) )
+
+passport.serializeUser( ( ID, done ) => done( null, ID ) ) // saves user id to session
+
+passport.deserializeUser( ( ID, done ) => {
+    const db = app.get( 'db' )
+    db.find_user_session( [ID] )
+    .then( user => {
+        done( null, user[0] )
+    } )
+} )
 
 // 70C
 massive( process.env.DB_CONNECTION ).then( db => { app.set( 'db', db ) } )
